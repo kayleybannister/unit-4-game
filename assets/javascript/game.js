af8a1acb57dfcf8fd408db2009ee37e413743c52
Kayley Bannister <kayley.bannister@gmail.com>
2019-06-24 17:02:14 -0400

    added link to access JS file

---
+++ assets/javascript/game.js
@@ -0,0 +1,5 @@
+//creating the randomNumber variable so that the values can only be between 19 and 120
+var randomNumber = Math.floor(Math.random() * (120-19+1) + 19);
+
+//creating variable for each crystal value to be between 1 and 12
+var crystalValue = Math.floor(Math.random() * (12 - 1) + 1);
