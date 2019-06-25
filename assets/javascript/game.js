2e09434822389c0467b0bcbb95b4395c0c7d4135
Kayley Bannister <kayley.bannister@gmail.com>
2019-06-25 09:17:42 -0400

    created variables for each crystal color and a variable for the score to be updated

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,8 +1,19 @@
 console.log("The javascript is now running");
 
+//DECLARING VARIABLES
+
 //variables for wins and losses
 var wins = 0;
 var losses = 0;
+
+//need to create variables for the crystals
+var blueCrystal;
+var greenCrystal;
+var redCrystal;
+var purpleCrystal;
+
+//variable for score
+var score;
 
 //creating the randomNumber variable so that the values can only be between 19 and 120
 var randomNumber = Math.floor(Math.random() * (120-19+1) + 19);
