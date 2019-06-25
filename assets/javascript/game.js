4126a4e1f29af874e87b120517be57e5883cff18
Kayley Bannister <kayley.bannister@gmail.com>
2019-06-25 09:20:36 -0400

    revised variables for randomNumber and crystalValue. Set both to 0, will need to create functions for the random values to be assigned to both

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -13,10 +13,10 @@ var redCrystal;
 var purpleCrystal;
 
 //variable for score
-var score;
+var score = 0;
 
-//creating the randomNumber variable so that the values can only be between 19 and 120
+//randomNumber variable - will need to create a function so that the values can only be between 19 and 120
 var randomNumber = Math.floor(Math.random() * (120-19+1) + 19);
 
-//creating variable for each crystal value to be between 1 and 12
-var crystalValue = Math.floor(Math.random() * (12 - 1) + 1);
+//value of each crystal - will need function for value to be between 1 and 12
+var crystalValue = 0;
