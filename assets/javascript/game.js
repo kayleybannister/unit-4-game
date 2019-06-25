95366d04ad035299ecb4984526ce070ef00f38b0
Kayley Bannister <kayley.bannister@gmail.com>
2019-06-25 12:16:23 -0400

    created function to run the entire game and hopefully it will restart the game

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -10,54 +10,77 @@ var losses = 0;
 var randomNumber = Math.floor(Math.random() * (120-19+1) + 19);
 console.log(randomNumber);
 
-//value of each crystal
+//variable to generate a random value of each crystal
 var blueCrystal = Math.floor(Math.random() * (12-1+1) + 1);
-    console.log("Blue Crystal Value: " + blueCrystal);
-
 var greenCrystal = Math.floor(Math.random() * (12-1+1) + 1);
-    console.log("Green Crystal Value: " + greenCrystal);
-
 var purpleCrystal = Math.floor(Math.random() * (12-1+1) + 1);
-    console.log("Purple Crystal Value: " + purpleCrystal);
-
 var redCrystal = Math.floor(Math.random() * (12-1+1) + 1);
-    console.log("Red Crystal Value: " + redCrystal);
 
+//holds the value for the total score every time a crystal is clicked
 var score = 0;
 
-$(document).ready(function() {
+//creating runGame function to start the game each time
+var runGame = function() {
 
+blueCrystal;
+    console.log("Blue Crystal Value: " + blueCrystal);
 
-$("#random-number").text(randomNumber);
+greenCrystal;
+    console.log("Green Crystal Value: " + greenCrystal);
 
+purpleCrystal;
+    console.log("Purple Crystal Value: " + purpleCrystal);
+
+redCrystal;
+    console.log("Red Crystal Value: " + redCrystal);
+
+//using jQuery to display the random number that the user must match to win the game
+$("#random-number").html(randomNumber);
+$("#score").html(score);
+
+    //displays alert when blue crystal is clicked
     $("#blue").on("click", function() {
 
         alert("You clicked the blue crystal");
     });
 
+    //displays alert when green crystal is clicked
     $("#green").on("click", function() {
 
         alert("You clicked the green crystal");
     });
 
+    //displays alert when purple crystal is clicked
     $("#purple").on("click", function() {
 
         alert("You clicked the purple crystal");
     });
 
+    //displays alert when red crystal is clicked
     $("#red").on("click", function() {
         
         alert("You clicked the red crystal");
     });
 
 
+    //creating if/else statement so that if the score goes above the randomNumber, the user loses
+    //else if the score exactly equals the randomNumber, the user wins
+    //else, the user must continue playing until they either win or lose
     if (score > randomNumber) {
         alert("You have gone above the random number. You lose!");
+        losses++;
     }
 
     else if (score === randomNumber) {
         wins++;
     }
 
+    else {
+        console.log("You need to keep going!");
+    }
+
 //ends the entire function
-});
+};
+
+
+runGame();
