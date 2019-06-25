1631dcfa46ac2d59714384657195d09fe7f1e0bd
Kayley Bannister <kayley.bannister@gmail.com>
2019-06-25 10:11:08 -0400

    added click events when each crystal is clicked, the number is displayed in the score and added if/else statement if the score goes above the random number

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -22,3 +22,39 @@ console.log(randomNumber);
 //value of each crystal
 var crystalValue = Math.floor(Math.random() * (12-1+1) + 1);
 console.log(crystalValue);
+
+$(document).ready(function() {
+
+
+$("#random-number").text(randomNumber);
+
+    $("#blue").on("click", function() {
+        blueCrystal = crystalValue;
+        $("#score").text(crystalValue);
+    });
+
+    $("#green").on("click", function() {
+        greenCrystal = crystalValue;
+        $("#score").text(crystalValue);
+    });
+
+    $("#purple").on("click", function() {
+        purpleCrystal = crystalValue;
+        $("#score").text(crystalValue);
+    });
+
+    $("#red").on("click", function() {
+        redCrystal = crystalValue;
+        $("#score").text(crystalValue);
+    });
+
+    if (crystalValue > randomNumber) {
+        alert("You have gone above the random number. You lose!");
+        losses++;
+    }
+
+    else if (crystalValue === randomNumber) {
+        wins++;
+    }
+//ends the entire function
+});
